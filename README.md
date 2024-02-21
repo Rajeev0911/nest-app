86848de0471c4c7b0d1b3f9710504c0729c4e687
Rajeev <rajeevgid2023@gmail.com>
2024-02-21 21:06:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### This is a basic nestJs crud application which is connected to postgresql database.
+
+In app.module.ts file, replace the username, password and db_name with your own postgresql username, password and database name.
+
+To run : npm run start
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './controllers/user.controller';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'username',
+      password: 'password',
+      database: 'db_name',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+})
+export class AppModule {}
